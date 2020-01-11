811978c49f7a75c2d13a3dff1e443dccaf9cc65a
Deepa Thiagarajan <deepa.name@gmail.com>
2020-01-11 10:55:59 -0800

    move state to props

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -7,20 +7,8 @@ import {
   CardBody,
   CardTitle
 } from 'reactstrap';
-import CampsiteInfo from './CampsiteInfoComponent';
 
 class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedCampsite: null
-    };
-  }
-
-  onCampsiteSelect(campsite) {
-    this.setState({ selectedCampsite: campsite });
-  }
-
   renderSelectedCampsite(campsite) {
     if (campsite) {
       return (
@@ -40,7 +28,7 @@ class Directory extends Component {
     const directory = this.props.campsites.map(campsite => {
       return (
         <div key={campsite.id} className='col-md-5 m-1'>
-          <Card onClick={() => this.onCampsiteSelect(campsite)}>
+          <Card onClick={() => this.props.onClick(campsite.id)}>
             <CardImg width='100%' src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
@@ -53,7 +41,6 @@ class Directory extends Component {
     return (
       <div className='container'>
         <div className='row'>{directory}</div>
-        <CampsiteInfo campsite={this.state.selectedCampsite} />
       </div>
     );
   }
